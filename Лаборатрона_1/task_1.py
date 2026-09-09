a7d2492195a1d505ad38004748e4072de9a9bc95
Ілля Квятківський <kviatkivskyi.illia@chnu.edu.ua>
2026-09-09 08:56:14 +0300

    Виправив перше завдання.

--- Лаборатрона_1/task_1.py
+++ Лаборатрона_1/task_1.py
@@ -1,9 +1,9 @@
 import math
 
 m = float(input("Enter m: "))
-z = float(input("Enter z: "))
 
-right_side = 1 / math.sqrt(m + 2)
-
-print(f"Z = {z}")
-print(f"1/sqrt(m+2) = {right_side}")
+if m > -2:
+    z = 1 / math.sqrt(m + 2)
+    print(f'z = {z}')
+else:
+    print('Помилка: m має бути більше ніж -2')
